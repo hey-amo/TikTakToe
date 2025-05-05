8c4b2cd15ef173daf8d6febe38dc008546e1a398
AD <silentflute.ad@gmail.com>
2025-05-05 10:28:53 +0100

    Creating project

---
+++ TikTakToe/TikTakToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTakToe
+//
+//  Created by Amarjit on 05/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTakToe/TikTakToe/TikTakToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTakToeApp.swift
+//  TikTakToe
+//
+//  Created by Amarjit on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TikTakToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
